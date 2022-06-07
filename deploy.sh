994b122f3443bd0e10820083374d41ec9066effe
Ryan Sonshine <ryansonshine@users.noreply.github.com>
2022-06-07 21:36:25 +0700

    chore(serverless): remove deprecated fields

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ npm install --prefix ./service-A & npm install --prefix ./service-B &&
 echo 'Done Installing Dependencies'
 
 echo 'Deploying Service B'
-npm run deploy --prefix ./service-B
+npm run deploy --prefix ./service-B &&
 
 echo 'Deploying Service A'
 npm run deploy --prefix ./service-A
